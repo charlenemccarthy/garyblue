38bc36552dced1cd1960d76dbb910a3f31ccdf57
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-06-24 13:35:38 -0230

    JS File Created

---
+++ addToCart.js
@@ -0,0 +1,11 @@
+//  Author: Chelsea Slade (Mayne) & Charlene McCarthy
+// Assignment: Sprint 1
+// Date: June 28, 2024
+
+//Select Item
+
+//Adjust Quantity
+
+//Remove from Cart
+
+//Calculate Price
